e1a76ccff00902b4ceb055ea97ddd96383838942
Vamshi039 <vamshikittu039@gmail.com>
2022-08-02 16:18:53 +0530

    added the project

---
+++ student-management-system/src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -0,0 +1,47 @@
+package net.javaguides.sms.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.service.StudentService;
+
+@Controller
+public class StudentController {
+
+	StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
+	}
+	
+	//handler method to list students and return model and view
+	
+	@GetMapping("/students")
+	public String listStudents(Model model){
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
+	
+	@GetMapping("/students/new")
+	public String createStudentForm(Model model) {
+		//create student object to hold student form data
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "create_student";
+	}
+	
+	@PostMapping("/students")
+	public String saveStudent(@ModelAttribute("student") Student student) {
+		studentService.saveStudent(student);
+		return "redirect:/students";
+		
+	}
+}
